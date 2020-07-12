06dd6ea822d45b9f32d572b18b4ec0318e796bb5
ManaswiniKundeti <manaswini.kundeti@gmail.com>
2020-07-12 13:57:39 -0400

    Stetho Initialized

---
+++ app/src/main/java/com/manu/foodacious/FoodaciousApplication.kt
@@ -0,0 +1,12 @@
+package com.manu.foodacious
+
+import android.app.Application
+import com.facebook.stetho.Stetho
+
+class FoodaciousApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Stetho.initializeWithDefaults(this)
+    }
+
+}
