138ade28ea26d7bb1b9aad8641455a72589f67ba
ManaswiniKundeti <manaswini.kundeti@gmail.com>
2020-07-18 15:57:29 -0400

    Fetching users location implemented

--- app/src/main/java/com/manu/foodacious/view/ui/SplashActivity.kt
+++ app/src/main/java/com/manu/foodacious/view/ui/SplashActivity.kt
@@ -3,17 +3,23 @@ package com.manu.foodacious.view.ui
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.manu.foodacious.R
 
 class SplashActivity: AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
 
     companion object{
         private const val LOCATION_PERMISSION_REQUEST_CODE = 100
+        private val TAG = SplashActivity::class.java.simpleName
     }
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +30,7 @@ class SplashActivity: AppCompatActivity(), ActivityCompat.OnRequestPermissionsRe
 
     private fun checkLocationEnabled() {
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fetchUsersLocation()
         } else {
             requestLocationPermission()
@@ -32,7 +38,7 @@ class SplashActivity: AppCompatActivity(), ActivityCompat.OnRequestPermissionsRe
     }
 
     private fun requestLocationPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
             Toast.makeText(this,
                 "Grant Foodacious location permission to get recommendations in your city!",
                 Toast.LENGTH_LONG).show()
@@ -41,7 +47,7 @@ class SplashActivity: AppCompatActivity(), ActivityCompat.OnRequestPermissionsRe
         // Location permission has not been granted yet, request it.
         ActivityCompat.requestPermissions(
             this,
-            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
             LOCATION_PERMISSION_REQUEST_CODE
         )
     }
@@ -64,5 +70,14 @@ class SplashActivity: AppCompatActivity(), ActivityCompat.OnRequestPermissionsRe
 
     private fun fetchUsersLocation() {
         Toast.makeText(this, "Location permission granted", Toast.LENGTH_SHORT).show()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        try {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                Log.d(TAG, "Location fetched: ${location.latitude}")
+            }
+        } catch (e: SecurityException) {
+            Log.e(TAG, "Security exception", e)
+        }
     }
 }
